f4d15c9937abd729c7a30554c2569266566260bc
Lucija Krsticevic <lkrstice1@pmfst.hr>
2023-06-09 23:38:22 +0200

    Dodane boje

--- constants/Boje.js
+++ constants/Boje.js
@@ -2,5 +2,7 @@ export default {
     Naslov: "#900C3F",
     Tekst: "#581845",
     Tipka: "#581845",
-    Bijela: "#FFFFFF"
+    Bijela: "#FFFFFF",
+    Jezik: "#E4F7C1",
+    Pozadina: "#FAFAFC"
 }
