fc58a8d4cda883405f7d8f3a9e27dc12bacbf025
Lucija Krsticevic <lkrstice1@pmfst.hr>
2023-06-09 23:43:02 +0200

    Ekran za odabir razloga

---
+++ screens/Razlog.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import Boje from "../constants/Boje";
+import JezikBtn from "../components/JezikBtn";
+
+const Razlog = () => {
+    return(
+        <View style={stil.ekran}>
+            <Text style={stil.naslov}>Razlog</Text>
+            <JezikBtn title="Prijatelji/obitelj" onPress = { () => {console.log("Klik")} } />
+            <JezikBtn title="Posao/škola" onPress = { () => {console.log("Klik")} } />
+            <JezikBtn title="Hobi" onPress = { () => {console.log("Klik")} } />
+            <JezikBtn title="Ostalo" onPress = { () => {console.log("Klik")} } />
+        </View>
+    )
+}
+
+const stil = StyleSheet.create({
+    ekran: {
+        alignItems: 'center',
+        marginTop: 180,
+        backgroundColor: Boje.Pozadina,
+
+    },
+    naslov: {
+        fontSize: 20,
+        fontFamily: 'FaunaOne-Regular',
+        color: Boje.Naslov,
+    }
+})
+
+export default Razlog
