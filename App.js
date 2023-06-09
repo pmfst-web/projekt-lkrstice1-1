596b2fea731779a5edb35e4caad3e9682edd1d05
Lucija Krsticevic <lkrstice1@pmfst.hr>
2023-06-09 19:40:02 +0200

    Dodan pocetni zaslon

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import PocetniEkran from './screens/PocetniEkran';
+
+import * as Font from 'expo-font'
+import AppLoading from 'expo-app-loading'
+
+const dohvatiFont = () => {
+    return Font.loadAsync ({
+        'ConcertOne-Regular': require('./assets/fonts/ConcertOne-Regular.ttf'),
+        'FaunaOne-Regular': require('./assets/fonts/FaunaOne-Regular.ttf'),
+    })
+}
 
 export default function App() {
+  const [podaciUcitani, postaviPodatke] = useState(false)
+
+  if(!podaciUcitani){
+    return(
+      <AppLoading 
+        startAsync = {dohvatiFont}
+        onFinish = {() => {postaviPodatke(true)}}
+        onError = {(err) => console.error(err)}
+      />
+    )
+  }
+
   return (
     <View>
-      <Text>Pocetak</Text>
+      <PocetniEkran />
     </View>
   );
 }
