73e98fc684ba5f04704f56bb2529e68831b1f492
Lucija Krsticevic <lkrstice1@pmfst.hr>
2023-06-10 19:34:04 +0200

    Model podataka

---
+++ models/izraz.js
@@ -0,0 +1,10 @@
+class Izraz {
+    constructor(jezik, razlog, predznanje, fraza){
+        this.jezik = jezik;
+        this.razlog = razlog;
+        this.predznanje = predznanje;
+        this.fraza = fraza;
+    }
+}
+
+export default Izraz
