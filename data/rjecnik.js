4ef7654d1724169bab580c4a565e5f50d91cc759
Lucija Krsticevic <lkrstice1@pmfst.hr>
2023-06-10 19:32:35 +0200

    Podaci

---
+++ data/rjecnik.js
@@ -0,0 +1,12 @@
+import Izraz from "../models/izraz";
+
+export const IZRAZI = [
+    new Izraz('eng', 'obitelj', 'pocetnik', 'Parents'),
+    new Izraz('eng', 'obitelj', 'posjeduje', 'Relative'),
+    new Izraz('eng', 'posao', 'pocetnik', 'Team player'),
+    new Izraz('eng', 'posao', 'posjeduje', 'The big picture'),
+    new Izraz('eng', 'hobi', 'pocetnik', 'Fun'),
+    new Izraz('eng', 'hobi', 'posjeduje', 'Entertainment'),
+    new Izraz('eng', 'ostalo', 'pocetnik', 'Other'),
+    new Izraz('eng', 'ostalo', 'posjeduje', 'Rest'),
+]
