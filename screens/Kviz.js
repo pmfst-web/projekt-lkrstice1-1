aaa96aa27d53ee5d54b6da23f27f7c01e11a49ad
Lucija Krsticevic <lkrstice1@pmfst.hr>
2023-06-13 03:56:31 +0200

    Filtriranje izraza

--- screens/Kviz.js
+++ screens/Kviz.js
@@ -1,21 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet, Text, FlatList } from 'react-native';
 import Btn from '../components/Btn';
-import { IZRAZI } from '../data/rjecnik';
+import { useSelector } from 'react-redux';
+import izrazReducer from '../store/reducers/izraz';
+import Unos from '../components/Unos';
 
 const Kviz = ({ route, navigation }) => {
     const {jezik, razlog, predznanje} = route.params
+    const odabirIzraza = useSelector(izrazReducer)
+    const [unos, setUnos] = ''
 
     const prikazIzraza = (podaci) => {
-        return(
-            <Text>{podaci.item.fraza}</Text>
-        )
+        if(podaci.item.jezik === jezik && podaci.item.razlog === razlog && podaci.item.predznanje === predznanje){
+            return <Text style={stil.izrazT}>{podaci.item.fraza}</Text>
+        }
     }
+
     return(
         <View style={stil.ekran}>
+            <FlatList data={odabirIzraza} renderItem={prikazIzraza} style={stil.izraz}/>
+            <Unos value={unos} onChangeText={provjeraPrijevoda()}/>
             <Btn title="Odabir jezika" onPress={() => navigation.navigate('Odabir')} />
-            <Text>{jezik}, {razlog}, {predznanje}</Text>
-            <FlatList data={IZRAZI} renderItem={prikazIzraza} />
         </View>
     )
 }
@@ -23,7 +28,16 @@ const stil = StyleSheet.create({
     ekran: {
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    izraz: {
+        paddingTop: 150,
+        paddingBottom: 50,
+    },
+    izrazT: {
+        fontFamily: 'FaunaOne-Regular',
+        fontSize: 45
     }
+
 })
 
 export default Kviz
