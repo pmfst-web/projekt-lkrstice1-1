e8e916e055911739ece8e742cdef0a39e0ac89b7
Lucija Krsticevic <lkrstice1@pmfst.hr>
2023-06-13 03:54:52 +0200

    Zajednicki spremnik

---
+++ store/reducers/izraz.js
@@ -0,0 +1,10 @@
+import { IZRAZI } from "../../data/rjecnik"
+
+const izrazReducer = (a, b, c) => {
+    if(a === 'eng' && b === 'obitelj' && c === 'pocetnik'){
+        return IZRAZI
+    }
+    return IZRAZI
+}
+
+export default izrazReducer
