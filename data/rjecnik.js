b3e27513496006994b2126d8217050f8dbff1545
Lucija Krsticevic <lkrstice1@pmfst.hr>
2023-06-13 03:56:13 +0200

    Promjena modela; dodan prijevod

--- data/rjecnik.js
+++ data/rjecnik.js
@@ -1,12 +1,12 @@
 import Izraz from "../models/izraz";
 
 export const IZRAZI = [
-    new Izraz('eng', 'obitelj', 'pocetnik', 'Parents'),
-    new Izraz('eng', 'obitelj', 'posjeduje', 'Relative'),
-    new Izraz('eng', 'posao', 'pocetnik', 'Team player'),
-    new Izraz('eng', 'posao', 'posjeduje', 'The big picture'),
-    new Izraz('eng', 'hobi', 'pocetnik', 'Fun'),
-    new Izraz('eng', 'hobi', 'posjeduje', 'Entertainment'),
-    new Izraz('eng', 'ostalo', 'pocetnik', 'Other'),
-    new Izraz('eng', 'ostalo', 'posjeduje', 'Rest'),
+    new Izraz('eng', 'obitelj', 'pocetnik', 'Parents', 'roditelji'),
+    new Izraz('eng', 'obitelj', 'posjeduje', 'Relative', 'rodbina'),
+    new Izraz('eng', 'posao', 'pocetnik', 'Co-worker', 'Suradnik'),
+    new Izraz('eng', 'posao', 'posjeduje', 'Contributor', 'Suradnik'),
+    new Izraz('eng', 'hobi', 'pocetnik', 'Fun', 'Zabava'),
+    new Izraz('eng', 'hobi', 'posjeduje', 'Entertainment', 'Zabava'),
+    new Izraz('eng', 'ostalo', 'pocetnik', 'Other', 'Drugo'),
+    new Izraz('eng', 'ostalo', 'posjeduje', 'Rest', 'Ostatak'),
 ]
