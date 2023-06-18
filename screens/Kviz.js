4c4f3d475fa165f5731f6446fb0039cb87710da2
Lucija Krsticevic <lkrstice1@pmfst.hr>
2023-06-19 00:29:20 +0200

    Dodan reducer, random izbor rejči s obzirom na komponente te provjera unosa

--- screens/Kviz.js
+++ screens/Kviz.js
@@ -1,70 +1,105 @@
-import React, { useState } from 'react';
-import { View, StyleSheet, Text, FlatList, Button, TextInput } from 'react-native';
+import React, { useState, useEffect, useMemo } from 'react';
+import { View, StyleSheet, Text, TextInput, Alert } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
 import Btn from '../components/Btn';
-import { useSelector } from 'react-redux';
-import izrazReducer from '../store/reducers/izraz';
+import { dodajIzraze } from '../store/reducers/izraz';
+import { IZRAZI } from '../data/rjecnik';
 
 const Kviz = ({ route, navigation }) => {
-    const [prijevod, setPrijevod] = useState('Pocetak');
-    const {jezik, razlog, predznanje} = route.params
-    const odabirIzraza = useSelector(izrazReducer)
+  const [prijevod, setPrijevod] = useState('');
+  const { jezik, razlog, predznanje } = route.params;
+  const dispatch = useDispatch();
+  const izrazi = useSelector((state) => state.izrazi);
 
-    const prikazIzraza = (podaci) => {
-        if(podaci.item.jezik === jezik && podaci.item.razlog === razlog && podaci.item.predznanje === predznanje){
-            const provjera = (a, b) => {
-                if(a === b){
-                    return "Tocan odgovor"
-                }
-                return "Pokusajte ponovno"
-            }
-            return (
-                <View style={stil.ekran}>
-                    <Text style={stil.izrazT}>{podaci.item.niz[Math.floor(Math.random()*podaci.item.niz.length)][0]}</Text>
-                    <TextInput 
-                        style={stil.unos}
-                        placeholder='Unesi prijevod'
-                        onChangeText = {(val) => setPrijevod(val)}
-                    />
-                    <Btn
-                        title='Provjeri'
-                        onPress = {() => alert(provjera(podaci.item.niz[2][1], prijevod))}
-                    />
-                </View>
-            )
-        }
+  useMemo(() => {
+    dispatch(dodajIzraze(IZRAZI));
+  }, [dispatch]);
+
+  const filtriraniIzrazi = useMemo(() => {
+    return izrazi.filter((izraz) => izraz.jezik === jezik && izraz.razlog === razlog && izraz.predznanje === predznanje);
+  }, [izrazi, jezik, razlog, predznanje]);
+
+  const randomIndeks = useMemo(() => {
+    if (filtriraniIzrazi.length > 0) {
+      return Math.floor(Math.random() * filtriraniIzrazi.length);
     }
+    return null;
+  }, [filtriraniIzrazi]);
 
-    return(
+  const provjeriOdgovor = (izraz, prijevod) => {
+    if (izraz[1].toLowerCase() === prijevod.toLowerCase()) {
+      return 'Točan odgovor!';
+    }
+    return 'Netočan prijevod, pokušajte ponovno!';
+  };
+
+  const izraz = useMemo(() => {
+    if (randomIndeks !== null) {
+      const izraz = filtriraniIzrazi[randomIndeks];
+      return izraz.niz[Math.floor(Math.random() * izraz.niz.length)];
+    }
+    return null;
+  }, [randomIndeks, filtriraniIzrazi]);
+
+  useEffect(() => {
+    setPrijevod('');
+  }, [izraz]);
+
+  const provjeriPrijevod = () => {
+    if (prijevod.trim() === '') {
+      Alert.alert('Unesite prijevod!');
+    } else {
+      Alert.alert(provjeriOdgovor(izraz, prijevod));
+    }
+  };
+
+  const prikazIzraza = () => {
+    if (izraz !== null) {
+      return (
         <View style={stil.ekran}>
-            <FlatList data={odabirIzraza} renderItem={prikazIzraza} style={stil.izraz}/>
-            <Btn title="Odabir jezika" onPress={() => navigation.navigate('Odabir')} />
+          <Text style={stil.izrazT}>{izraz[0]}</Text>
+          <TextInput
+            style={stil.unos}
+            placeholder="Unesi prijevod"
+            onChangeText={(text) => setPrijevod(text)}
+            value={prijevod}
+          />
+          <Btn
+            title="Provjeri"
+            onPress={provjeriPrijevod}
+          />
         </View>
-    )
-}
+      );
+    }
+  };
+
+  return (
+    <View style={stil.ekran}>
+      {prikazIzraza()}
+      <Btn title="Odabir jezika" onPress={() => navigation.navigate('Odabir')} />
+    </View>
+  );
+};
 
 const stil = StyleSheet.create({
-    ekran: {
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    izraz: {
-        paddingTop: 150,
-        paddingBottom: 50,
-    },
-    izrazT: {
-        fontFamily: 'FaunaOne-Regular',
-        fontSize: 45,
-        paddingBottom: 50
-    },
-    unos: {
-        borderWidth: 1,
-        borderColor: "#777",
-        width: 200,
-        height: 50,
-        marginBottom: 20,
-        paddingLeft: 20
-    }
+  ekran: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 70
+  },
+  izrazT: {
+    fontFamily: 'FaunaOne-Regular',
+    fontSize: 30,
+    paddingBottom: 50,
+  },
+  unos: {
+    height: 40,
+    width: 250,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 30,
+    paddingHorizontal: 10,
+  },
+});
 
-})
-
-export default Kviz
+export default Kviz;
