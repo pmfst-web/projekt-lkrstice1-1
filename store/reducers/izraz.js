1537347974c815a6166eca9363321b0b2f3dc051
Lucija Krsticevic <lkrstice1@pmfst.hr>
2023-06-19 00:30:49 +0200

    Reducer

--- store/reducers/izraz.js
+++ store/reducers/izraz.js
@@ -1,7 +1,23 @@
 import { IZRAZI } from "../../data/rjecnik"
 
-const izrazReducer = () => {
-    return IZRAZI
-}
+const initialState = IZRAZI;
 
-export default izrazReducer
+const DODAJ_IZRAZE = 'DODAJ_IZRAZE';
+
+const izrazReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case DODAJ_IZRAZE:
+      return [...state, ...action.payload];
+    default:
+      return state;
+  }
+};
+
+export const dodajIzraze = (izrazi) => {
+  return {
+    type: DODAJ_IZRAZE,
+    payload: izrazi,
+  };
+};
+
+export default izrazReducer;
